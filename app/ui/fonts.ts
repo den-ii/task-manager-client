5b98098ba5dd709c5c80bc3ce590e737688e0f78
Deni <deni@Tobis-MacBook-Pro.local>
2024-05-22 14:29:43 +0100

    complete login page ui

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Poppins, Rubik } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const poppins = Poppins({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+});
+export const rubik = Rubik({ subsets: ["latin"] });
